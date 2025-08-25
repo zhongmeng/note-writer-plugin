89f492c19ac81c0bc545c3d4110b56577dc13b26
仲儒 孟 <zhongmeng1734@outlook.com>
2025-08-25 21:31:12 +0800

    init

--- main.ts
+++ main.ts
@@ -1,134 +1,52 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, MarkdownView, Notice } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+export default class NoteWriterPlugin extends Plugin {
+  async onload() {
+    this.addCommand({
+      id: 'write-table-to-note',
+      name: '将表格数据写入笔记',
+      callback: () => this.writeDataToNote({}),
+    });
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+    this.registerDomEvent(window, 'message', (event: MessageEvent) => {
+      if (event.data.type === 'write-to-note') {
+        this.writeDataToNote(event.data.payload);
+      }
+    });
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+    new Notice('Note Writer Plugin 已加载');
+  }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+  async writeDataToNote(payload: any) {
+    const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+    if (!activeView) {
+      new Notice('无活跃笔记');
+      return;
+    }
 
-	async onload() {
-		await this.loadSettings();
+    const file = activeView.file;
+    if (!file) return;
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    let content = await this.app.vault.read(file);
+    const expenses = payload.expenses || [];
+    let total = expenses.reduce((sum: number, exp: any) => sum + (exp.amount || 0), 0);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+    let table = '\n## 支出记录（从表格写入）\n\n| 日期 | 描述 | 金额 | 类别 |\n|------|------|------|------|\n';
+    expenses.forEach((exp: any) => {
+      table += `| ${exp.date} | ${exp.description} | ${exp.amount} | ${exp.category} |\n`;
+    });
+    table += `| **总计** | | **${total.toFixed(2)}** | |\n`;
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+    const marker = '## 支出记录（从表格写入）';
+    const markerIndex = content.indexOf(marker);
+    if (markerIndex !== -1) {
+      const endIndex = content.indexOf('\n##', markerIndex + 1) || content.length;
+      content = content.slice(0, markerIndex) + table + content.slice(endIndex);
+    } else {
+      content += table;
+    }
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    await this.app.vault.modify(file, content);
+    new Notice('数据已写入笔记');
+  }
 }
